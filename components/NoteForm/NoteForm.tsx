38319f8bda80fdbb11ca0eac542e487ebf9851ff
victoriaovcharova <victoria.ovcharova93@gmail.com>
2025-08-17 21:50:22 +0300

    hw8

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,98 +1,107 @@
-'use client'
+'use client';
+import css from './NoteForm.module.css';
+import { useId } from 'react';
+import { useRouter } from 'next/navigation';
+import { useMutation } from '@tanstack/react-query';
+import { createNote } from '@/lib/api';
+import { useNoteDraftStore } from '@/lib/store/noteStore';
+import type { NewNote } from '@/types/note';
 
-import css from "./NoteForm.module.css";
-import { useMutation} from "@tanstack/react-query";
-import { createNote } from "@/lib/api";
-import { useRouter } from "next/navigation";
-import { NewNoteData } from "@/types/note";
-import { useNoteDraftStore } from "@/lib/store/noteStore";
+const tags = ['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'];
 
+const NoteForm = () => {
+  const router = useRouter();
+  const id = useId();
 
-export default function NoteForm () {
-    const router = useRouter();
+  const handleClickCancel = () => router.push('/notes/filter/all');
 
-    const { draft, setDraft, clearDraft } = useNoteDraftStore();
+  const { draft, setDraft, clearDraft } = useNoteDraftStore();
 
-    const handleChange = (
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
-    ) => {
-        setDraft({
-            ...draft,
-            [event.target.name]: event.target.value,
-        });
-    };
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
+    setDraft({ ...draft, [event.target.name]: event.target.value });
+  };
 
-    
-     const mutation = useMutation({
-         mutationFn: createNote,
-         onSuccess: () => {
-             clearDraft();
-             router.push('/notes/filter/All');
-         },
-         onError: (error) => {
-            console.error('Create note failed:', error);
-          },
-     });
-    
-    const handleSubmit = (formData: FormData) => {
-        const values = Object.fromEntries(formData) as NewNoteData;
-        mutation.mutate(values);
-    };
+  const { mutate } = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      clearDraft();
+      router.push('/notes/filter/all');
+    },
+  });
 
-    const handleCancel = () => router.push('/notes/filter/All');
+  const handleSubmit = (formData: FormData) => {
+    const data = Object.fromEntries(formData) as unknown as NewNote;
+    // const data: NewNote = {
+    //   title: formData.get('title') as string,
+    //   content: formData.get('content') as string,
+    //   tag: formData.get('tag') as NewNote['tag'],
+    // };
+    // console.log('form data', data);
 
+    mutate(data);
+  };
 
-    return (
-        <form action={handleSubmit} className={css.form}>
-        
-                    <div className={css.formGroup}>
-                        <label htmlFor="title">Title</label>
-                <input type="text"
-                    name="title"
-                    className={css.input}
-                    defaultValue={draft?.title}
-                    onChange={handleChange}
-                    required
-                     />
-        
-                    </div>
+  return (
+    <form className={css.form} action={handleSubmit}>
+      <div className={css.formGroup}>
+        <label htmlFor={`${id}-title`}>Title</label>
+        <input
+          id={`${id}-title`}
+          type="text"
+          name="title"
+          className={css.input}
+          defaultValue={draft?.title}
+          onChange={handleChange}
+        />
+        {/* <span name="title" className={css.error} /> */}
+      </div>
 
-                    <div className={css.formGroup}>
-                        <label htmlFor="content">Content</label>
-                        <textarea
-            name="content"
-                            rows={8}
-                    className={css.textarea}
-                    defaultValue={draft?.content}
-                    onChange={handleChange}
-                        />
-            
-                    </div>
+      <div className={css.formGroup}>
+        <label htmlFor={`${id}-content`}>Content </label>
+        <textarea
+          id={`${id}-content`}
+          name="content"
+          rows={8}
+          className={css.textarea}
+          defaultValue={draft?.content}
+          onChange={handleChange}
+        />
 
-                    <div className={css.formGroup}>
-                        <label htmlFor="tag">Tag</label>
-                        <select id="tag" name="tag" className={css.select} defaultValue={draft?.tag} onChange={handleChange}>
-                            <option value="Todo">Todo</option>
-                            <option value="Work">Work</option>
-                            <option value="Personal">Personal</option>
-                            <option value="Meeting">Meeting</option>
-                            <option value="Shopping">Shopping</option>
-                        </select>
-                    </div>
+        {/* <span name="content" className={css.error} /> */}
+      </div>
 
-                    <div className={css.actions}>
-                       <button
-                            type="submit"
-                    className={css.submitButton}
-    >
-                            Create note
-                        </button>
-                        <button
-                            type="button"
-                            onClick={handleCancel}
-                            className={css.cancelButton}
-                        >Cancel</button>
-                    </div>
-        </form>
-    );
+      <div className={css.formGroup}>
+        <label htmlFor={`${id}-tag`}>Tag</label>
+        {
+          <select
+            id={`${id}-tag`}
+            name="tag"
+            className={css.select}
+            defaultValue={draft?.tag}
+            onChange={handleChange}
+          >
+            {tags.map((tag) => (
+              <option value={tag} key={tag}>
+                {tag}
+              </option>
+            ))}
+          </select>
+        }
+        {/* <span name="tag" className={css.error} /> */}
+      </div>
+
+      <div className={css.actions}>
+        <button type="button" className={css.cancelButton} onClick={handleClickCancel}>
+          Cancel
+        </button>
+        <button type="submit" className={css.submitButton}>
+          Create note
+        </button>
+      </div>
+    </form>
+  );
 };
+
+export default NoteForm;
