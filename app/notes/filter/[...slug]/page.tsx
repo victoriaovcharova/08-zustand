eb6cbe44dde9fa45988d7aacab9e32a3da04b971
victoriaovcharova <victoria.ovcharova93@gmail.com>
2025-08-17 21:23:21 +0300

    hw8

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,45 +1,49 @@
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
-import type { Metadata } from "next";
+import { Metadata } from "next";
 
-type PageProps = { params: { slug: string[] } };
 
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-  const raw = params.slug[0];
-  const tag = raw === "All" ? "All" : raw;
-
-  const title = `Notes: ${tag}`;
-  const description = `${tag} notes list`;
-
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      url: `https://08-zustand-ten-kappa.vercel.app/notes/filter/${encodeURIComponent(tag)}`,
-      images: [
-        {
-          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-          width: 1200,
-          height: 630,
-          alt: "NoteHub Open Graph image",
-        },
-      ],
-      type: "article",
-    },
-  };
+type Props = {
+  params: Promise<{ slug: string[] }>;
 }
 
-export default async function FilteredNotesPage({ params }: PageProps) {
-  const raw = params.slug[0];
-  const tag = raw === "All" ? undefined : raw;
+export async function generateMetadata({ params }: Props) : Promise<Metadata> {
+  const { slug } = await params;
+  const tag = slug[0] === "All" ? undefined : slug[0];
+  return {
+    title: `Notes: ${tag}`,
+    description: `${tag} notes list`,
+    openGraph: {
+      title: `Notes: ${tag}`,
+      description: `${tag} notes list`,
+      url: `https://08-zustand-ten-kappa.vercel.app/notes/filter/${tag}`,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: "NoteHub logo",
+        }
+      ],
+      type: 'article',
+    }
+  };
+  
+};
+
+export default async function FilteredNotesPage({ params }: Props) {
+  const { slug } = await params;
+
+  const tag = slug[0] === "All" ? undefined : slug[0];
 
   const initialNotes = await fetchNotes({
-    search: "",
+    search: '',
     page: 1,
-    ...(tag ? { tag } : {}),
-  });
-
-  return <NotesClient initialNotes={initialNotes} tag={tag ?? "All"} />;
+    perPage: 12,
+  tag
+});
+  
+  return (
+    <NotesClient initialNotes={initialNotes} tag={tag || 'All'} />
+  );
 }
