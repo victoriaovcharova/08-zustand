c48cff46fbed25f2e728ba4084605564ab0c0938
victoriaovcharova <victoria.ovcharova93@gmail.com>
2025-08-17 20:44:57 +0300

    hw8

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,26 +1,35 @@
-import css from "@/components/NoteFoundStyle/noteFound.module.css"
-import { Metadata } from "next";
+import Link from 'next/link';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "NotFound",
-  description: "There is nothing here.",
+  title: "404 Page",
+  description: "Page you are looking for does not exist",
   openGraph: {
-  title: "NotFound",
-  description: "There is nothing here.",
-    url: "/",
-    images: [{
-      url:"https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-      width: 1200,
-      height: 630,
-      alt: "NoteHub Image",
-    }]
+    title: "404 Page",
+    description: "Page you are looking for does not exist",
+    url: "https://08-zustand-ten-kappa.vercel.app/",
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: "notehub",
+      },
+    ],
+    type: "article",
   }
 };
 
 
-export default function NotFound() {
-        return <>
-                <h1 className={css.title}>404 - Page not found</h1>
-                <p className={css.description}>Sorry, the page you are looking for does not exist.</p> 
-        </>
-} 
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link href="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
