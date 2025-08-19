3577cafaa1bee94c1fdb4064fce2408c8686727b
victoriaovcharova <victoria.ovcharova93@gmail.com>
2025-08-19 14:44:11 +0300

    hw8

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -1,46 +1,35 @@
-'use client'
-import { useState } from "react";
-import css from "./TagsMenu.module.css";
-import Link from "next/link";
+'use client';
+import css from './TagsMenu.module.css';
+import { useState } from 'react';
+import Link from 'next/link';
 
-const TagsMenu = () => {
+
+type Props = {
+  tags: string[];
+};
+
+function TagsMenu({ tags }: Props) {
   const [isOpen, setIsOpen] = useState(false);
-
-
-  const tags = [
-    "All",
-    "Todo",
-    "Work",
-    "Personal",
-    "Meeting",
-    "Shopping",
-  ];
-  const openTags= () => {
-    setIsOpen(!isOpen)
-  }
-
-  const handleClickLink = () =>{
-    setIsOpen(false)
-  }
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div className={css.menuContainer}>
-      <button className={css.menuButton} onClick={openTags}>
-        {isOpen ? "Notes ▴" : "Notes ▾"}
+      <button className={css.menuButton} onClick={toggle}>
+        Notes ▾
       </button>
-        {isOpen && (
-            <ul className={css.menuList}>
-        {tags.map((tag) => (
-          <li className={css.menuItem} key={tag}>
-            <Link
-              href={`/notes/filter/${tag}`}
-              className={css.menuLink}
-              onClick={handleClickLink}
-            >{tag}</Link>
-          </li>))}
-              </ul>)}
+      {isOpen && (
+        <ul className={css.menuList}>
+          {tags.map((tag) => (
+            <li key={tag} className={css.menuItem}>
+              <Link href={`/notes/filter/${tag}`} className={css.menuLink} onClick={toggle}>
+                {tag}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-};
+}
 
 export default TagsMenu;
--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,13 +1,15 @@
-"use client";
-import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+'use client';
+
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 type Props = {
   children: React.ReactNode;
 };
-export default function TanStackProvider({ children }: Props) {
-  const [queryClient] = useState(() => new QueryClient());
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-}
+const TanStackProvider = ({ children }: Props) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
+
+export default TanStackProvider;
